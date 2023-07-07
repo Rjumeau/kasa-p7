5bdbf17787a97ef4822b7bde26f4a54876765c90
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-07-07 12:02:33 +0200

    footer done

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,10 @@
+import logoFooter from '../../assets/Images/logo-footer.svg'
+
 function Footer() {
   return (
     <div class="footer">
+      <img src={ logoFooter } alt="Logo Kasa Footer" className="logo-footer" />
+      <p>© 2020 Kasa. All rights reserved</p>
     </div>
   )
 }
