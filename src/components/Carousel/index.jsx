8b4e99736769fc1470337b9f5fdf46e8afd823d6
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-08-03 16:46:35 +0200

    mobile version finished

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 import './Carousel.scss'
 
