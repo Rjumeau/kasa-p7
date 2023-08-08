49c4bbe2112131d41573424417c3cb3e5c215ea7
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-08-08 18:13:47 +0200

    move router logic in an app file, project finished

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Accomodation from './pages/Accomodation';
+import About from './pages/About';
+
+import Error from './components/Error';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      < div className="container">
+        < Navbar />
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/accomodations/:accomodationId" element={<Accomodation />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={<Error />} />
+          </Routes>
+      </div>
+      < Footer />
+    </Router>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './assets/Styles/main.scss'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
-import Home from './pages/Home'
-import Accomodation from './pages/Accomodation'
-import About from './pages/About'
-
-import Error from './components/Error'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-      <Router>
-        < div className="container">
-          < Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/accomodations/:accomodationId" element={<Accomodation />} />
-                <Route path="/about" element={<About />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
-        </div>
-        < Footer />
-      </Router>
+    < App />
   </React.StrictMode>,
 document.getElementById('root')
 )
